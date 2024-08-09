b2a0f73498e1ba05bb774f7d585bf62b47d398c1
Fesuy <fesuy@192.168.100.5>
2024-08-09 17:19:02 +0700

    Add line number and some logic

--- lib.js
+++ lib.js
@@ -1,44 +1,63 @@
 function hasSimilarChars (s) {
-    for (var i in s)
-      if (s[i - 1] === s[i] && i > 0) return true;
-  
-    return false;
+  for (var i in s)
+    if (s[i - 1] === s[i] && i > 0) return true;
+
+  return false;
+}
+
+function generate (params) {
+  var prefix = params.prefix || '';
+  var filterSimilarChars = params.filterSimilarChars || false;
+  var len = params.len;
+  var chars = params.chars;
+  var step = params.step;
+  var end = params.end;
+
+  if (prefix.length <= len)
+    if (step) step(prefix);
+
+  if (prefix.length >= len)
+    return [prefix];
+
+  var strings = [prefix];
+
+  for (var i in chars) {
+    var nextString = prefix + chars[i];
+
+    if (filterSimilarChars && hasSimilarChars(nextString))
+      continue;
+
+    var nextParams = {
+      prefix: nextString,
+      filterSimilarChars: filterSimilarChars,
+      len: len,
+      chars: chars,
+      step: step
+    };
+
+    strings = strings.concat(generate(nextParams));
   }
+
+  if (end) end(strings);
   
-  function bruteForce (params) {
-    var prefix = params.prefix || '';
-    var filterSimilarChars = params.filterSimilarChars || false;
-    var len = params.len;
-    var chars = params.chars;
-    var step = params.step;
-    var end = params.end;
-  
-    if (prefix.length <= len)
-      if (step) step(prefix);
-  
-    if (prefix.length >= len)
-      return [prefix];
-  
-    var strings = [prefix];
-  
-    for (var i in chars) {
-      var nextString = prefix + chars[i];
-  
-      if (filterSimilarChars && hasSimilarChars(nextString))
-        continue;
-  
-      var nextParams = {
-        prefix: nextString,
-        filterSimilarChars: filterSimilarChars,
-        len: len,
+  return strings;
+}
+
+function exec(chars, _2D, _3D, _4D, similar) {
+  let result = [];
+  for (let t of [_2D && 2, _3D && 3, _4D && 4]) {
+    if (t) {
+      for (let num of generate({
         chars: chars,
-        step: step
-      };
-  
-      strings = strings.concat(bruteForce(nextParams));
+        len: t,
+        filterSimilarChars: !similar
+      })) {
+        if (num.length == t) {
+          result.push(num);
+        }
+      }
     }
-  
-    if (end) end(strings);
-  
-    return strings;
   }
+
+  return result;
+}
